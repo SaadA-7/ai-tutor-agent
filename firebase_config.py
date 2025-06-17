e742857fe530ae919f5a24117875cd208245bb8f
Saad A. <saad7@terpmail.umd.edu>
2025-06-17 18:34:59 -0400

    test

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,11 @@
-import os
 import json
+import streamlit as st
 import firebase_admin
 from firebase_admin import auth as admin_auth, credentials
 
-# Load Firebase service account from Streamlit Cloud secret
+# Load Firebase service account from Streamlit secrets
 if not firebase_admin._apps:
-    service_account_info = json.loads(os.environ["firebase_service_account"])
+    service_account_info = json.loads(st.secrets["firebaseServiceAccount"])
     cred = credentials.Certificate(service_account_info)
     firebase_admin.initialize_app(cred)
 
