a01ae037a93160f3ceb27a78fe3f9b08d791510a
Saad A. <saad7@terpmail.umd.edu>
2025-06-20 19:21:03 -0400

    protect key

--- firebase_config.py
+++ firebase_config.py
@@ -23,13 +23,13 @@ db = firestore.client()
 
 # Pyrebase client setup (needed for user login/signup)
 firebase_config = {
-    "apiKey": "AIzaSyAJGJmTnkc7ybIEOFTfFXtONUCGK99gEUs",
-    "authDomain": "ai-tutor-agent-v1.firebaseapp.com",
-    "projectId": "ai-tutor-agent-v1",
-    "storageBucket": "ai-tutor-agent-v1.appspot.com",
-    "messagingSenderId": "36461042628",
-    "appId": "1:36461042628:web:6a6051217d94d88f9ee291",
-    "databaseURL": ""
+    "apiKey": st.secrets["firebase_config"]["apiKey"],
+    "authDomain": st.secrets["firebase_config"]["authDomain"],
+    "projectId": st.secrets["firebase_config"]["projectId"],
+    "storageBucket": st.secrets["firebase_config"]["storageBucket"],
+    "messagingSenderId": st.secrets["firebase_config"]["messagingSenderId"],
+    "appId": st.secrets["firebase_config"]["appId"],
+    "databaseURL": st.secrets["firebase_config"]["databaseURL"]
 }
 
 
